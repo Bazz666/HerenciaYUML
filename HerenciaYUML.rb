b5d756298088768822df994d68333181ecf04e18
Sergio Herrera <herrera.se1992@gmail.com>
2021-04-26 14:42:51 -0400

    clase reunion mensual y atributo day

--- HerenciaYUML.rb
+++ HerenciaYUML.rb
@@ -10,3 +10,26 @@ class Appointment
 
 
 end
+
+class MonthlyAppointment < Appointment
+
+    attr_accessor :day
+
+    def initialize (location, purpose, hour, min, day)
+        super(location, purpose, hour, day)
+        @day = day
+    end
+
+    def self.day()
+        day
+    end
+    
+    def occurs_on?(day)
+        self.day == day
+    end
+
+    def to_s
+        puts "Reunion mensual en #{@location} sobre #{@purpose} el dia #{@day} a la(s) #{@hour}:#{@min}"
+    end
+
+end
